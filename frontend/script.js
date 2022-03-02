e01b652f4d5b03ba9462bb553ccc3e6d8359d5f9
toth_szilvia <szylwy@gmail.com>
2022-03-02 15:55:31 +0100

    add fetch

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,34 @@
-const loadEvent =  _ => { // NEM FOGJUK A PARAMÉTEREKET HASZNÁLNI, EZÉRT VAN () HELYETT _
-    
+function Country(name, short, population, flag, continent){
+    this.name = name;
+    this.short = short;
+    this.population = population;
+    this.flag = flag;
+    this.continent = continent;
+}
+
+// Components
+const header = (logo) => {
+    return `
+    <header>
+        <a id="logo">${logo}</a>
+        <button></button>
+    </header>`;
+};
+
+const loadEvent = async  _ => { 
+    // get data
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+
+    // process data
+    let countries = countryArr.map(function (country){
+        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0]);
+    });
+
+    console.log(countries);
+
+    const root = document.getElementById("root");
+    root.insertAdjacentHTML(`beforeend`, header("Countries"));
 }
 
 window.addEventListener("load", loadEvent);
