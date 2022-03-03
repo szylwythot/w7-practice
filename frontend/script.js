b69394d01ef67148f75f78be3f686911db974e93
toth_szilvia <szylwy@gmail.com>
2022-03-03 16:43:20 +0100

    map and input

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,28 @@
 const form = () => {
     return `
     <form>
-        <div class="input1">
-            <input type="text" />
-        </div>
-        <div class="input2">
-            <input type="text" />
-        </div>
-        <div class="input3">
-            <input type="text" />
-        </div>
+        <input id="input1" name="input1" type="text" />
+        <input id="input2" name="input2" type="text" />
+        <input id="input3" name="input3" type="text" />
         <button>Send</button>
     </form>
     `;
 };
 
 function loadEvent() {
-console.log("load");
-const rootElement = document.getElementById("root");
+    console.log("load");
+    const rootElement = document.getElementById("root");
 
-rootElement.insertAdjacentHTML("beforeend", form());
+    rootElement.insertAdjacentHTML("beforeend", form());
 
-const inputList = document.querySelectorAll("input");
-for (const input of inputList) {
-    input.addEventListener("input", function(e) {
-        console.log(e.target.value);
-    })
-}
+    const inputList = document.querySelectorAll("input");
+    console.log(typeof inputList);
+    console.log(Array.isArray(inputList));
+
+    (Array.from(inputList)).map((input) =>{
+        input.addEventListener("input", function(e) {
+            console.log(e.target.value);
+        });
+    });
 }
 window.addEventListener("load", loadEvent);
