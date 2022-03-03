464c87107ac08dc9a8039d227c9402e3cfbba543
toth_szilvia <szylwy@gmail.com>
2022-03-03 15:08:18 +0100

    callback funtionnal

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,24 @@
-function init(){
-    var a = 50;
+function init(mathFunction){
+    var a = 5;
     var b = 6;
-
-    console.log(c);
     
     if(a < b){
-        (function (){
-            var c = b - a;
-        })();
+        let c = mathFunction(b, a);
+        console.log(c);
     }
     else{
-        (function (){
-            var c = a - b;
-        })();
+        let c = mathFunction(a, b);
+        console.log(c);
     }
     
-    console.log(c);
 }
 
-init();
+const initC = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+}
+
+const initD = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+
+init(initD);
