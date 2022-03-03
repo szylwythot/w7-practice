1d4806cdae83639cab354f46f24d87d51a60e523
toth_szilvia <szylwy@gmail.com>
2022-03-03 17:10:51 +0100

    add form  select

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,15 @@
-const form = () => {
+const formHtml = () => {
     return `
     <form>
-        <input id="input1" name="input1" type="text" />
-        <input id="input2" name="input2" type="text" />
-        <input id="input3" name="input3" type="text" />
-        <button>Send</button>
+    <input id="input1" name="input1" type="text" />
+    <input id="input2" name="input2" type="text" />
+    <input id="input3" name="input3" type="text" />
+    <select name="animals" id="animals">
+        <option value="both555">both</option>
+        <option value="cats555">cats</option>
+        <option value="dogs555">dogs</option>
+    </select>
+    <button>Send</button>
     </form>
     `;
 };
@@ -12,17 +17,30 @@ const form = () => {
 function loadEvent() {
     console.log("load");
     const rootElement = document.getElementById("root");
-
-    rootElement.insertAdjacentHTML("beforeend", form());
+    
+    rootElement.insertAdjacentHTML("beforeend", formHtml());
+    
+    const form = rootElement.querySelector("form");
 
     const inputList = document.querySelectorAll("input");
     console.log(typeof inputList);
     console.log(Array.isArray(inputList));
-
+    
     (Array.from(inputList)).map((input) =>{
-        input.addEventListener("input", function(e) {
-            console.log(e.target.value);
+        input.addEventListener("input", function(event) {
+            console.log(event.target.value);
         });
+    });
+
+    let select = form.querySelector("select");
+    select.addEventListener(`input`, (event) => {
+        console.log(event.target.value);
+    })
+    
+    form.addEventListener(`submit`, (event) => {
+        event.preventDefault();
+        console.log(event.target);
+        console.dir(event.target);
     });
 }
 window.addEventListener("load", loadEvent);
