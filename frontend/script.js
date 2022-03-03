b10ca26af405c7b16ad7a0df59e21c8bed5fce5d
toth_szilvia <szylwy@gmail.com>
2022-03-03 16:22:26 +0100

    add ;

--- frontend/script.js
+++ frontend/script.js
@@ -17,11 +17,11 @@ const form = () => {
 
 function loadEvent() {
 console.log("load");
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById("root");
 
-rootElement.insertAdjacentHTML("beforeend", form())
+rootElement.insertAdjacentHTML("beforeend", form());
 
-const inputList = document.querySelectorAll("input")
+const inputList = document.querySelectorAll("input");
 for (const input of inputList) {
     input.addEventListener("input", function(e) {
         console.log(e.target.value);
