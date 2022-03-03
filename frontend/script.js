5035b218367c3381a0e1bc1d166eedc45359f7d9
toth_szilvia <szylwy@gmail.com>
2022-03-03 15:31:14 +0100

    add input eventhandler

--- frontend/script.js
+++ frontend/script.js
@@ -7,14 +7,22 @@ function loadEvent(){
 
     let rootElement = document.getElementById("root");
     let formHtml =`
-    <form action="script.js">
+    <form action="">
         <label for="inputLaber">Input:</label><br>
         <input type="text" id="inputText" name="inputText" value=""><br>
+        <!-- <input type="submit" value="Submit"> -->
         <input type="submit" value="Submit">
-    </form> 
+    </form>
     `;
 
     rootElement.insertAdjacentHTML(`beforeend`, formHtml);
+
+    const input = document.getElementById('inputText');
+    input.addEventListener('input', updateValue);
+
+    function updateValue(e) {
+        console.log(e.target.value);
+    }
 }
 
 window.addEventListener("load", loadEvent);
