13b32428533aedd26d6ce24cbb4c903c1827528d
toth_szilvia <szylwy@gmail.com>
2022-03-03 14:49:33 +0100

    init c

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,23 @@
-function loadEvent(){
+function init(){
+    var a = 50;
+    var b = 6;
+
+    console.log(c);
     
+    if(a < b){
+        function initC(){
+            var c = b - a;
+        }
+        initC();
+    }
+    else{
+        function initC(){
+            var c = a - b;
+        }
+        initC();
+    }
+    
+    console.log(c);
 }
 
-window.addEventListener("load", loadEvent);
+init();
